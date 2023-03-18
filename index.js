fab580583931346a4b0c4c0a4b3af1432246de12
nazm0l <tanjirahmednazmul@gmail.com>
2023-03-19 00:54:41 +0600

    Vercel config file added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
-
 
 //middleware
 app.use(cors());
@@ -13,61 +12,61 @@ app.use(express.json());
 
 //MongoDB
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mbq1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-        try{
-            await client.connect();
-            const carCollection = client.db('nazmulCar').collection('cars');
+  try {
+    client.connect();
+    const carCollection = client.db("nazmulCar").collection("cars");
 
-            //total car
-            app.get('/cars', async(req, res) =>{
-              const query = {};
-              const cursor = carCollection.find(query);
-              const cars = await cursor.toArray();
-              res.send(cars);
-            });
+    //total car
+    app.get("/cars", async (req, res) => {
+      const query = {};
+      const cursor = carCollection.find(query);
+      const cars = await cursor.toArray();
+      res.send(cars);
+    });
 
-            //single car
-            app.get('/cars/:id', async(req, res) =>{
-              const id = req.params.id;
-              const query = {_id: ObjectId(id)};
-              const cars = await carCollection.findOne(query);
-              res.send(cars);
-            });
+    //single car
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const cars = await carCollection.findOne(query);
+      res.send(cars);
+    });
 
-            //add car
+    //add car
 
-            app.post('/cars', async(req, res) =>{
-              const newCar = req.body;
-              const result = await carCollection.insertOne(newCar);
-              res.send(result);
-            });
+    app.post("/cars", async (req, res) => {
+      const newCar = req.body;
+      const result = await carCollection.insertOne(newCar);
+      res.send(result);
+    });
 
-            //delete car
+    //delete car
 
-            app.delete('/cars/:id', async(req, res) => {
-              const id = req.params.id;
-              const query = {_id: ObjectId(id)};
-              const result = await carCollection.deleteOne(query);
-              res.send(result);
-            })
-        }
-        finally{
-
-        }
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
 //main
-app.get('/',(req, res) =>{
-    res.send('Running NazmulMon Server')
+app.get("/", (req, res) => {
+  res.send("Running NazmulMon Server");
 });
 
 app.listen(port, () => {
-    console.log('Listening port ',port);
-})
+  console.log("Listening port ", port);
+});
